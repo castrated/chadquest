6c085637c9c6a3736488c09bcb6e00ae1920baf2
castrated <47220664+castrated@users.noreply.github.com>
2020-02-07 17:04:06 -0500

    Create main.c

---
+++ main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main(){
+   printf("ChadQUEST\n");
+   printf("You wake up in a damp, cold room. There's a smooth, stone floor.\n");
+   printf("\nGoodbye!\n");
+   return(0);
+}
