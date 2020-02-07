cce6519c8a0d2b8d2493d1c027400fc85de1d164
castrated <47220664+castrated@users.noreply.github.com>
2020-02-07 17:20:51 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,26 @@
 #include <stdio.h>
-int main(){
+#include <string.h>
+#include "location.h"
+static char input[100];
+static int getInput(){
+  printf("\n--> ");
+  return(fgets(input,sizeof(input),stdin)!=NULL);
+}static int parseAndExecute(){
+  char *verb=strtok(input," \n");
+  char *noun=strtok(NULL," \n");
+  if(verb!=NULL){
+    if(strcmp(verb,"quit")==0){
+      return(0);
+    }else if(strcmp(verb,"look")==0){
+      executeLook(noun);
+    }else if(strcmp(verb,"go")==0){
+      executeGo(noun);}else{
+      printf("I don't know how to '%s'.\n",verb);
+    }
+  }return(1);
+}int main(){
    printf("ChadQUEST\n");
-   printf("You wake up in a damp, cold room. There's a smooth, stone floor.\n");
+   executeLook("around");
    printf("\nGoodbye!\n");
    return(0);
 }
