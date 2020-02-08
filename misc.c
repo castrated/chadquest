a055d79ae700fe338b4b15313ef796fc388bbb8c
castrated <47220664+castrated@users.noreply.github.com>
2020-02-08 13:07:04 -0500

    Update misc.c

--- misc.c
+++ misc.c
@@ -2,6 +2,10 @@
 #include <string.h>
 #include "object.h"
 #include "misc.h"
+OBJECT *getPassageTo(OBJECT *targetLocation){
+  OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){
+    if(obj->location==player->location&&obj->destination==targetLocation){
+      return(obj);}}return(NULL);}
 OBJECT *parseObject(const char *noun){
   OBJECT *obj,*found=NULL;
   for(obj=objs;obj<endOfObjs;obj++){
