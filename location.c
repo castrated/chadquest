2ca30d4de04a19bbfef886e9e184a5794523117b
castrated <47220664+castrated@users.noreply.github.com>
2020-02-08 13:04:07 -0500

    Update location.c

--- location.c
+++ location.c
@@ -2,21 +2,26 @@
 #include <string.h>
 #include "object.h"
 #include "misc.h"
-struct location{
-   const char *description;
-   const char *tag;
-}locs[]={
-   {"an open field","field"},
-   {"a little cave","cave"}
-};
-#define numberOfLocations(sizeof(locs)/sizeof(*locs))
-static unsigned locationOfPlayer=(0);
-void executeLook(const char *noun){if(noun!=NULL&&strcmp(noun,"around")==0){printf("You are in %s.\n",player->location->description);
-   listObjectsAtLocation(player->location);}else{
+void executeLook(const char *noun){
+   if(noun!=NULL&&strcmp(noun,"around")==0){
+      printf("You are in %s.\n",player->location->description);
+      listObjectsAtLocation(player->location);
+   }else{
       printf("I don't understand what you want to see.\n");
-   }
-}void executeGo(const char *noun){OBJECT *obj=parseObject(noun);if(obj==NULL){
-   printf("I don't know where you want to go.\n");}else if(obj==player->location){
-   printf("You're already there.\n");}else{
-   printf("Ok.\n");player->location=obj;executeLook("around");}
-}
+   }}void executeGo(const char *noun){
+   OBJECT *obj=parseObject(noun);
+   if(obj==NULL){
+      printf("I don't understand where you want to go.\n");
+   }else if(obj==player->location){
+      printf("You are already there.\n");
+   }else if(getPassageTo(obj)!=NULL){
+      printf("Ok.\n");
+      player->location=obj;
+      executeLook("around");
+   }else if(obj->location==player->location&&obj->destination!=NULL){
+      printf("Ok.\n");
+      player->location=obj->destination;
+      executeLook("around");
+   }else{
+      printf("You can't go there.\n");
+   }}
