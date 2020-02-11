b153582f0db987424ddadc124b765ca3f0b036eb
castrated <47220664+castrated@users.noreply.github.com>
2020-02-11 08:26:27 -0500

    Update object.c

--- object.c
+++ object.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include "object.h"
+static const char *tags0[]={"field",NULL};
+static const char *tags1[]={"cave",NULL};
+static const char *tags2[]={"silver","coin","silver coin",NULL};
+static const char *tags3[]={"gold","coin","gold coin",NULL};
+static const char *tags4[]={"guard","burly guard",NULL};
+static const char *tags5[]={"yourself",NULL};
+static const char *tags6[]={"east","entrance",NULL};
+static const char *tags7[]={"west","out",NULL};
+static const char *tags8[]={"west","north","south","forest",NULL};
+static const char *tags9[]={"east","north","south","rock",NULL};
 OBJECT objs[]={
    {"an open field","field",NULL,NULL},
    {"a little cave","cave",NULL,NULL},
