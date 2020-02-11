ca68313080a593298af0a321f010c6e39ac2913e
castrated <47220664+castrated@users.noreply.github.com>
2020-02-11 09:39:25 -0500

    Update inventory.c

--- inventory.c
+++ inventory.c
@@ -5,18 +5,35 @@ static void moveObject(const char *noun,OBJECT *from,OBJECT *to){
    OBJECT *obj=parseObject(noun);if(obj==NULL){
       printf("I don't understand what item you mean.\n");
    }else if(from!=obj->location){
-      printf("You can't.\n");}else if(to==NULL){
-      printf("There's nobody around to give that to.\n");
-   }else{
-      obj->location=to;printf("Ok.\n");
-   }}void executeGet(const char *noun){
-   moveObject(noun,player->location,player);
+      switch (distanceTo(obj)){case distPlayer:
+         printf("You should not be doing that to yourself.\n");
+         break;case distHeld:
+         printf("You already have %s.\n", obj->description);
+         break;case distLocation:
+      case distOverthere:
+         printf("That's not an item.\n");break;case distHere:
+         if(from==player){
+            printf("You have no %s.\n",noun);
+         }else{
+            printf("Sorry, %s has no %s.\n",from->description,noun);
+         }break;
+      case distHeldContained:
+      case distHereContained:
+         printf("Sorry, %s is holding it.\n",obj->location->description);
+         break;default:
+         printf("You don't see any %s here.\n",noun);}}else if(to==NULL){
+      printf("There is nobody here to give that to.\n");
+   }else if(obj->weight>to->capacity){
+      printf("That is way too heavy.\n");
+   }else if(obj->weight+weightOfContents(to)>to->capacity){
+      printf("That would become too heavy.\n");}else{
+      obj->location=to;
+      printf("OK.\n");
+   }}void executeGet(const char *noun){moveObject(noun,player->location,player);
 }void executeDrop(const char *noun){
-   moveObject(noun,player,player->location);
-}void executeGive(const char *noun){
-   moveObject(noun,player,personHere());
-}void executeAsk(const char *noun){
-   moveObject(noun,personHere(),player);
-}void executeInventory(void){
-  if(listObjectsAtLocation(player)==0){
-      printf("You're empty-handed.\n");}}
+   moveObject(noun,player,player->location);}void executeGive(const char *noun){
+   moveObject(noun,player,personHere());}void executeAsk(const char *noun){
+   moveObject(noun,personHere(),player);}void executeInventory(void){
+   if(listObjectsAtLocation(player)==0){
+      printf("You are empty-handed.\n");
+   }}
