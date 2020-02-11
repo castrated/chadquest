974a34ce8f19793b7272a51d7b44e26592b87c58
castrated <47220664+castrated@users.noreply.github.com>
2020-02-11 13:07:58 -0500

    Update execute.c

--- execute.c
+++ execute.c
@@ -1,31 +1,22 @@
 #include <stdio.h>
 #include "object.h"
 #include "misc.h"
-static int objectWithinReach(const char *verb,OBJECT *obj,const char *noun){
-   int ok=(0);
-   DISTANCE distance=distanceTo(obj);if(distance>distNotHere){
-      printf("I don't understand what you want to %s.\n",verb);
-   }else if(distance==distNotHere){
-      printf("You don't see any %s here.\n",noun);
-   }else if(distance>=distHereContained){
-      printf("That is out of reach.\n");
-   }else{
-      ok=(1);
-   }return(ok);}
-void executeOpen(const char *noun){
-   OBJECT *obj=parseObject(noun);
-   if(objectWithinReach("open",obj,noun)){
-      printf("%s",(*obj->open)(obj));
-   }}void executeClose(const char *noun){
-   OBJECT *obj=parseObject(noun);
-   if(objectWithinReach("close",obj,noun)){
-      printf("%s",(*obj->close)(obj));
-   }}void executeLock(const char *noun){
-   OBJECT *obj=parseObject(noun);
-   if(objectWithinReach("lock",obj,noun)){
-      printf("%s",(*obj->lock)(obj));
-   }}void executeUnlock(const char *noun){
-   OBJECT *obj=parseObject(noun);
-   if(objectWithinReach("unlock",obj,noun)){
-      printf("%s",(*obj->unlock)(obj));
-   }}
+#include "match.h"
+static int objectWithinReach(const char *verb, PARAM *par){
+   int ok=(0);if(par->distance>distNotHere){
+      printf("I don't understand what you want to %s.\n", verb);}else if(par->distance==distNotHere){
+      printf("You don't see any %s here.\n",par->tag);}else if(par->distance>=distHereContained){
+      printf("That is out of reach.\n");}else if(par->count>(1)){
+      printf("Multiple choices to %s; be more specific.\n",verb);}else{
+      ok=(1);}return(ok);
+}int executeOpen(void){
+   if(objectWithinReach("open",params)){
+      printf("%s",(*params->obj->open)(params->obj));}return(1);
+}int executeClose(void){
+   if(objectWithinReach("close",params)){
+      printf("%s",(*params->obj->close)(params->obj));}return(1);
+}int executeLock(void){if(objectWithinReach("lock",params)){
+      printf("%s",(*params->obj->lock)(params->obj));}return(1);
+}int executeUnlock(void){
+   if(objectWithinReach("unlock",params)){
+      printf("%s",(*params->obj->unlock)(params->obj));}return(1);}
