9eda930060fc4d2f3957472c08d29f49eb06568c
castrated <47220664+castrated@users.noreply.github.com>
2020-02-11 09:29:06 -0500

    Update location.c

--- location.c
+++ location.c
@@ -7,21 +7,34 @@ void executeLook(const char *noun){
       printf("You are in %s.\n",player->location->description);
       listObjectsAtLocation(player->location);
    }else{
-      printf("I don't understand what you want to see.\n");
-   }}void executeGo(const char *noun){
+      OBJECT *obj=parseObject(noun);DISTANCE distance=distanceTo(obj);if(distance>=distUnknownObject){
+         printf("I don't understand what you want to see\n");}else if(distance==distNotHere){
+         printf("You don't see any %s here.\n");}else if(distance==distOverthere){
+         printf("Too far away, move closer please.\n");}else if(distance==distHereContained){
+         printf("Hard to see; try to get it first.\n");}else{
+         printf(""%s",obj->details);listObjectsAtLocation(obj);
+                }
+   }}
+static void movePlayer(OBJECT *passage){
+   printf("%s",passage->textGo);
+   if(passage->destination != NULL){
+      player->location=passage->destination;
+      printf("\n");
+      executeLook("around");
+   }}
+void executeGo(const char *noun){
    OBJECT *obj=parseObject(noun);
-   if(obj==NULL){
+   DISTANCE distance=distanceTo(obj);
+   if(distance>=distUnknownObject){
       printf("I don't understand where you want to go.\n");
-   }else if(obj==player->location){
+   }else if(distance==distLocation){
       printf("You are already there.\n");
-   }else if(getPassageTo(obj)!=NULL){
-      printf("Ok.\n");
-      player->location=obj;
-      executeLook("around");
-   }else if(obj->location==player->location&&obj->destination!=NULL){
-      printf("Ok.\n");
-      player->location=obj->destination;
-      executeLook("around");
+   }else if(distance==distOverthere){
+      movePlayer(getPassageTo(obj));
+   }else if(distance==distHere){
+      movePlayer(obj);
+   }else if(distance<distNotHere){
+      printf("You can't get any closer than this.\n");
    }else{
-      printf("You can't go there.\n");
+      printf("You don't see any %s here.\n",noun);
    }}
