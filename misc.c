48cde99ce66ad6ddcb1c0bf34bcba3b17159ff35
castrated <47220664+castrated@users.noreply.github.com>
2020-02-11 09:48:21 -0500

    Update misc.c

--- misc.c
+++ misc.c
@@ -3,28 +3,40 @@
 #include "object.h"
 #include "misc.h"
 OBJECT *getPassageTo(OBJECT *targetLocation){
-  OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){
-    if(obj->location==player->location&&obj->destination==targetLocation){
-      return(obj);}}return(NULL);}
-OBJECT *parseObject(const char *noun){
-  OBJECT *obj,*found=NULL;
-  for(obj=objs;obj<endOfObjs;obj++){
-    if(noun!=NULL&&strcmp(noun,obj->tag)==0){
-      found=obj;
-    }
-  }return(found);
+   OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){if(obj->location==player->location&&obj->prospect==targetLocation){
+         return obj;}}return(NULL);
+}DISTANCE distanceTo(OBJECT *obj){
+   return
+      obj==NULL                                 ? distUnknownObject :
+      obj==player                               ? distPlayer :
+      obj==player->location                     ? distLocation :
+      obj->location==player                     ? distHeld :
+      obj->location==player->location           ? distHere :
+      getPassageTo(obj)!=NULL                   ? distOverthere :
+      obj->location==NULL                       ? distNotHere :
+      obj->location->location==player           ? distHeldContained :
+      obj->location->location==player->location ? distHereContained :
+                                                    distNotHere;
+}static int nounIsInTags(const char *noun,const char **tags){
+   while(*tags!=NULL){
+      if(strcmp(noun,*tags++)==(0))return(1);
+   }return(0);
+}OBJECT *parseObject(const char *noun){
+   OBJECT *obj,*found=NULL;for(obj=objs;obj<endOfObjs;obj++){if(noun!=NULL&&nounIsInTags(noun,obj->tags)&&distanceTo(obj)<distanceTo(found)){
+         found=obj;}}return(found);
 }OBJECT *personHere(void){
-  OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){
-    if(obj->location==player->location&&obj==guard){
-      return(obj);}}return(NULL);}
-int listObjectsAtLocation(OBJECT *location){
-  int count=0;
-  OBJECT *obj;
-  for(obj=objs;obj<endOfObjs;obj++){
-    if(obj!=player&&obj->location==location){
-      if(count++==0){
-        printf("You see:\n");
-      }printf("%s\n",obj->description);
-    }
-  }return(count);
+   OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){if(distanceTo(obj)==distHere&&obj->health>0){
+         return(obj);
+      }}return(NULL);
+}int listObjectsAtLocation(OBJECT *location){
+   int count=(0);OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){
+      if(obj!=player&&obj->location==location){if(count++==0){
+            printf("%s:\n",location->contents);
+         }printf("%s\n",obj->description);
+      }}return(count);
+}int weightOfContents(OBJECT *container){
+   int sum=(0);
+   OBJECT *obj;for(obj=objs;obj<endOfObjs;obj++){
+      if(obj->location==container)sum+=obj->weight;
+   }return(sum);
 }
