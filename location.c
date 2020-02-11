7dc180f7712f6bae5971ccc87d89ca22cccf2065
castrated <47220664+castrated@users.noreply.github.com>
2020-02-11 13:16:18 -0500

    Update location.c

--- location.c
+++ location.c
@@ -2,39 +2,40 @@
 #include <string.h>
 #include "object.h"
 #include "misc.h"
-void executeLook(const char *noun){
-   if(noun!=NULL&&strcmp(noun,"around")==0){
-      printf("You are in %s.\n",player->location->description);
-      listObjectsAtLocation(player->location);
+#include "match.h"
+int executeLookAround(void){
+   printf("You are in %s.\n", player->location->description);
+   listObjectsAtLocation(player->location);
+   return(1);
+}int executeLook(void){
+   if(params->distance>=distUnknownObject){
+      printf("I don't understand what you want to see.\n");
+   }else if(params->distance==distNotHere){
+      printf("You don't see any %s here.\n", params->tag);}else if(params->distance==distOverthere){
+      printf("Too far away, move closer please.\n");
+   }else if(params->distance==distHereContained){
+      printf("Hard to see, try to get it first.\n");
    }else{
-      OBJECT *obj=parseObject(noun);DISTANCE distance=distanceTo(obj);if(distance>=distUnknownObject){
-         printf("I don't understand what you want to see\n");}else if(distance==distNotHere){
-         printf("You don't see any %s here.\n");}else if(distance==distOverthere){
-         printf("Too far away, move closer please.\n");}else if(distance==distHereContained){
-         printf("Hard to see; try to get it first.\n");}else{
-         printf(""%s",obj->details);listObjectsAtLocation(obj);
-                }
-   }}
-static void movePlayer(OBJECT *passage){
+      printf("%s", params->obj->details);
+      listObjectsAtLocation(params->obj);
+   }return(1);
+}static void movePlayer(OBJECT *passage){
    printf("%s",passage->textGo);
-   if(passage->destination != NULL){
+   if(passage->destination!=NULL){
       player->location=passage->destination;
       printf("\n");
-      executeLook("around");
-   }}
-void executeGo(const char *noun){
-   OBJECT *obj=parseObject(noun);
-   DISTANCE distance=distanceTo(obj);
-   if(distance>=distUnknownObject){
+      executeLookAround();
+   }}int executeGo(void){
+   if(params->distance>=distUnknownObject){
       printf("I don't understand where you want to go.\n");
-   }else if(distance==distLocation){
+   }else if(params->distance==distLocation){
       printf("You are already there.\n");
-   }else if(distance==distOverthere){
-      movePlayer(getPassageTo(obj));
-   }else if(distance==distHere){
-      movePlayer(obj);
-   }else if(distance<distNotHere){
+   }else if(params->distance==distOverthere){
+      movePlayer(getPassageTo(params->obj));
+   }else if(params->distance==distHere){
+      movePlayer(params->obj);
+   }else if(params->distance<distNotHere){
       printf("You can't get any closer than this.\n");
    }else{
-      printf("You don't see any %s here.\n",noun);
-   }}
+      printf("You don't see any %s here.\n",params->tag);
+   }return(1);}
